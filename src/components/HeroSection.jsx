f0fd33445d8a7542fcd70c2a106d255e05363395
hughes-curtis <119542134+hughes-curtis@users.noreply.github.com>
2025-04-13 15:00:30 +0300

    fix button z index

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -12,7 +12,7 @@ const HeroSection = ({ el }) => {
         <section className="md:text-white flex flex-col items-center text-center md:w-1/2">
           <div className="text-3xl md:text-4xl font-bold mb-4 md:mb-12"><span>Experience </span><span ref={el} className='auto-type text-amber-500 min-h-[3em]'></span></div>
           <p className="text-lg mb-6">We provide professional WiFi solutions, CCTV installations, and complete network systems for homes and businesses.</p>
-          <a href={`https://wa.me/+254720973059`} target="_blank" rel="noopener noreferrer" className="bg-amber-600 font-bold text-white px-4 py-2 rounded-sm hover:bg-amber-700 z-50">Get a Free Quote</a>
+          <a href={`https://wa.me/+254720973059`} target="_blank" rel="noopener noreferrer" className="bg-amber-600 font-bold text-white px-4 py-2 rounded-sm hover:bg-amber-700 z-30">Get a Free Quote</a>
         </section>
       </div>
   );
